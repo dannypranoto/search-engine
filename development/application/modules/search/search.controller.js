50b7e84c1bbf9be866e4268bc91249dfbe704d5a
Danny Pranoto <danny.pranoto@veritrans.co.id>
2017-04-27 19:30:04 +0700

    Apply some fixes

--- development/application/modules/search/search.controller.js
+++ development/application/modules/search/search.controller.js
@@ -5,42 +5,43 @@
 
   function SearchController(apiService) {
     var vm = this;
-    initController();
 
-    function initController() {
-      vm.pagination = {
-        pageIndex: 1,
-        pageSize: 1,
-        totalItems: 10
-      }
-      vm.status = {
-        showSearchResult: true,
-        loadingSearchProduct: false
-      }
-      vm.searchList = [
-        {
-          title: 'Tes 1',
-          description: 'lalalala'
-        },
-        {
-          title: 'Tes 1',
-          description: 'lalalala'
-        },
-        {
-          title: 'Tes 1',
-          description: 'lalalala'
-        },
-        {
-          title: 'Tes 1',
-          description: 'lalalala'
-        }
-      ]
+    vm.pagination = {
+      pageIndex: 1,
+      pageSize: 1,
+      totalItems: 10
     }
+    vm.status = {
+      showSearchResult: true,
+      loadingSearchProduct: false
+    }
+    vm.searchQuery = ''
+    vm.searchList = [
+      {
+        title: 'Tes 1',
+        description: 'lalalala'
+      },
+      {
+        title: 'Tes 1',
+        description: 'lalalala'
+      },
+      {
+        title: 'Tes 1',
+        description: 'lalalala'
+      },
+      {
+        title: 'Tes 1',
+        description: 'lalalala'
+      }
+    ]
+    vm.searchProduct = searchProduct;
 
     function searchProduct() {
+      var endpoint = ''
+      var resource = ''
       if (vm.searchQuery) {
         vm.status.loadingSearchProduct = true;
-        apiService.get(ENV.prismApiEndpoint, 'products_search', {
+        apiService.get(endpoint, resource, {
           query: vm.searchQuery,
           size: vm.pagination.pageSize,
           from: vm.pagination.pageSize * (vm.pagination.pageIndex - 1)
@@ -59,7 +60,6 @@
         })
       }
     }
-
 
   }
 })();
